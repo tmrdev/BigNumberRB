e9e88ca70c388aface10af94560306d3024c4b10
Tim Reynolds <tmrdev@users.noreply.github.com>
2023-05-08 16:01:11 -0400

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png" width="25%" />
+    <RunningBalance msg="Vue in CodeSandbox!" />
+  </div>
+</template>
+
+<script>
+import RunningBalance from "./components/RunningBalance";
+export default {
+  name: "App",
+  components: {
+    RunningBalance,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/RunningBalance.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h3>Running Balance Tests</h3>
+  </div>
+</template>
+
+<script>
+import BigNumber from "bignumber.js";
+export default {
+  name: "RunningBalance",
+  props: {
+    msg: String,
+  },
+
+  mounted() {
+    console.log("** BigNumber **");
+    const nb = BigNumber(0.7);
+    const nb2 = nb.minus(0.1);
+    console.log(nb2.toString());
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+a {
+  color: #42b983;
+}
+</style>
